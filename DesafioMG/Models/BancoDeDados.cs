cd2754ad1d22fcd7d2e9bc7fe96d028481608c26
felip <felip@DESKTOP-NQUO750>
2022-04-29 16:12:49 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioMG/Models/BancoDeDados.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesafioMG.Models
+{
+    public class BancoDeDados : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Cliente> Livros { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=DESKTOP-NQUO750\SQLEXPRESS;Database=DesafioMG;Integrated Security=True");
+        }
+    }
+}
---
+++ DesafioMG/Models/Cliente.cs
@@ -0,0 +1,14 @@
+﻿namespace DesafioMG.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public int Telefone { get; set; }
+
+    }
+}
